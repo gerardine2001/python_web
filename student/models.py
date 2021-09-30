1bc4bf1f782f1e81c9e8d5e874265ce9c8584282
gerardine <gerardinekaba@gmail.com>
2021-09-30 13:17:33 +0300

    deployment

---
+++ student/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    first_name= models.CharField(
+        max_length=12)
+    last_name= models.CharField(
+        max_length=12)
+    age= models.PositiveIntegerField()
+    date_of_birth=models.DateTimeField()
+        
